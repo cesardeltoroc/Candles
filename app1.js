e4edc97829d35ebdac52ea7aa30ddc04a8c36068
Cesar Deltoro <cesardeltoro@Cesars-MacBook-Air.local>
2021-11-18 15:18:47 -0600

    Moved all javascript files to 1 tab and fix yes answer on email prompt

--- app1.js
+++ app1.js
@@ -1,10 +1,13 @@
-// let userAnswer= prompt ('Welcome Please Insert Name')
-// alert(userAnswer)
-// document.write ('Welcome, ' + userAnswer);
+let userAnswer= prompt ('Welcome Please Insert Name')
 
-function userName(){
-    let userAnswer= prompt ('Welcome Please Insert Name')
-     // alert(userAnswer)
+document.write ('Welcome, ' + userAnswer);
+
+function userSign () {
+    let userSign= prompt ("Are you interested in signing up for email notficitations for future sales and markdown? All sales are final and when on markdown")
+   
+    if(userSign.toUpperCase() == "YES" ){
+        userSign= prompt ("Please Insert Your Email:")
+    }
 
 }
-userName()
+userSign ();
--- app2.js
+++ app2.js
@@ -1,5 +1,9 @@
 function userSign () {
-    let userSign= prompt ('Whats your sign')
-    alert(userSign)
+    let userSign= prompt ("Are you interested in signing up for email notficitations for future sales and markdown? All sales are final and when on markdown")
+   
+    if (userSign.equals("yes")){
+        userSign= prompt ("Please Insert Your Email:")
+    }
+
 }
-userSign()
+userSign ();
