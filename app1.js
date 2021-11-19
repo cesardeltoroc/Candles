17e218522fab72f022e320f35554496f863e2a62
Cesar Deltoro <cesardeltoro@Cesars-MacBook-Air.local>
2021-11-18 21:25:28 -0600

    Updated JavaScript file adding random # guessing game and text inside of line 32 prompting user to sign up for email notifications

--- app1.js
+++ app1.js
@@ -1,13 +1,37 @@
-let userAnswer= prompt ('Welcome Please Insert Name')
+function guessingGame() {
+    let userAnswer = prompt('Guess a # from 1-10');
+    let correctAnswer = 2;   //may be a random number later on
+    let attempts = 4;
 
-document.write ('Welcome, ' + userAnswer);
+    for(let i =1; i < attempts; i++ ){
+        while(userAnswer < 1 || userAnswer > 10){
+        userAnswer = prompt('Please try again 1-10?');
+        }
+        if(userAnswer == correctAnswer){
+            alert('Congradulations you got the answer!');
+            break;
+        } else if (userAnswer > correctAnswer){
+            userAnswer = prompt('Sorry. Too high go lower');
+        } else if (userAnswer < correctAnswer){
+            userAnswer = prompt('Sorry the number is too low');
+        }
+        if(i == 7){
+            alert('The correct answer was' + correctAnswer);
+        }
+    }
+}
+
+
+let userName= prompt ('Welcome Please Insert Name')
+
+document.write ('Welcome, ' + userName);
+
 
 function userSign () {
-    let userSign= prompt ("Are you interested in signing up for email notficitations for future sales and markdown? All sales are final and when on markdown")
+    let userSign= prompt ("Are you interested in signing up for email notficitations for future sales and markdown, type YES if so? All sales are final and when on markdown. Type NO if you are NOT interested and you will be Redirected to the website.")
    
     if(userSign.toUpperCase() == "YES" ){
         userSign= prompt ("Please Insert Your Email:")
     }
 
 }
-userSign ();
